08f3a67543b5987a91bdec37b865b4ab535abc79
Dylan Silva <rogdylan98@gmail.com>
2021-12-02 12:31:18 -0600

    phase 1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import configureStore from './Store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
 
 function Root() {
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
+
   );
 }
 
